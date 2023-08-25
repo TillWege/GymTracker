63c9034e9a4f1dfa0377143fcac88b60a5de2d43
TillWege <tillwege@gmail.com>
2023-08-26 00:55:32 +0200

    Remove Tailwind CSS and Integrate Mantine UI
    The codebase is transitioned from Tailwind CSS to Mantine UI as per needs for more sophisticated UI components which Mantine provides out of the box. Tailwind related configuration and dependencies have been removed. Necessary Mantine dependencies are added. Emotion is also added for more advanced CSS-in-JS capabilities. MantineProvider is integrated in _app. Changes are also made in .eslintrc and prettier.config to ensure linting and formatting works with the new setup.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -24,6 +29,26 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'function',
+        format: ['PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    "react-hooks/exhaustive-deps": "off",
   },
 };
 
